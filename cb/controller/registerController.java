ece425c18e41ff720d1b81899ca1fab92b9ea886
aina4 <95197155+aina4@users.noreply.github.com>
2022-01-30 01:40:28 +0800

    Add files via upload

---
+++ cb/controller/registerController.java
@@ -0,0 +1,59 @@
+package cb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cb.dao.CustomerDAO;
+import cb.model.Customer;
+
+/**
+ * Servlet implementation class registerController
+ */
+@WebServlet("/registerController")
+public class registerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private CustomerDAO dao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public registerController() {
+        super();
+        dao = new CustomerDAO();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Customer c = new Customer();
+		
+		//retrieve input and set name, u/name, phone, email, p/w
+		c.setName(request.getParameter("name"));
+		c.setUsername(request.getParameter("username"));
+		c.setPhoneNum(request.getParameter("phoneNum"));
+		c.setEmail(request.getParameter("email"));		
+		c.setPassword(request.getParameter("password"));
+				
+		c = CustomerDAO.getUser(c);
+		//check if user exists
+		if(!c.isValid()){
+		   	try {
+		       		//if user not exist, add/register the user
+					dao.add(c);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		       	//redirect to login page
+		       	response.sendRedirect("signIn.jsp");
+		 }         
+	}
+
+}
