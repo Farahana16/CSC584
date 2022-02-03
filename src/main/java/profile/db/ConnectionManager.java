c7cef95701e62dd7f6223df0558359dc881c3879
Nurul Mutmainnah <95833706+yuyuhere@users.noreply.github.com>
2022-02-03 14:53:51 +0800

    Add files via upload

--- src/main/java/profile/db/ConnectionManager.java
+++ src/main/java/profile/db/ConnectionManager.java
@@ -1,31 +1,32 @@
-package profile.db;
+package payment.db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class ConnectionManager {
-	
-		    static Connection con;
-			private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-			private static final String DB_CONNECTION = "jdbc:mysql://localhost/glamping";
-			private static final String DB_USER = "root";
-			private static final String DB_PASSWORD = "";
+	    static Connection con;
+		private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+		private static final String DB_CONNECTION = "jdbc:mysql://localhost/grouproject";
+		private static final String DB_USER = "root";
+		private static final String DB_PASSWORD = "";
+		
+		public static Connection getConnection() {
 			
-			public static Connection getConnection() {
+			try {
+				
+				Class.forName(DB_DRIVER);
 				
 				try {
-					
-					Class.forName(DB_DRIVER);
-					
-					try {
-						con = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
-						System.out.println("Connected");
-					}catch(SQLException e){
-						e.printStackTrace();
-					}
-				}catch(ClassNotFoundException e) {
+					con = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
+					System.out.println("Connected");
+				}catch(SQLException e){
 					e.printStackTrace();
 				}
-				return con;
+			}catch(ClassNotFoundException e) {
+				e.printStackTrace();
 			}
-	
+			return con;
+		}
+		
 }
