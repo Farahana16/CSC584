c90c831d626c5737829a6c29d48486b7a60b5de9
nsyafiqah0 <95832450+nsyafiqah0@users.noreply.github.com>
2022-02-03 21:24:37 +0800

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,10 @@
+package booking.model;
+
+public class Customer {
+	private int custID;
+	private String custName;
+	private String custUsername;
+	private String custEmail;
+	private String custPwd;
+	private int custPhonenum;
+}
